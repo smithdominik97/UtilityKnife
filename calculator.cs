954d8ad297cb2851d642897f9dc3654e37a2487c
benmcleeod <benmcleod1@hotmail.com>
2023-10-11 12:21:23 +0100

    added new methods

--- calculator.cs
+++ calculator.cs
@@ -6,5 +6,18 @@ namespace UtilityKnife.Calculator
         {
             return x + y;
         }
+
+        public static int Subtract (int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multiply (int x, int y)
+        {
+            return x * y;
+        }
+        public static int Division (int x, int y)
+        {
+            return x / y;
+        }
     }
 }
